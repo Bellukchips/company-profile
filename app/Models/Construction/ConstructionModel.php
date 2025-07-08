f17243817936d9a335459dcc5fcb80a575324f63
bellukchips <bellukchips@gmail.com>
2025-07-08 18:04:47 +0700

    fix

--- app/Models/Construction/ConstructionModel.php
+++ app/Models/Construction/ConstructionModel.php
@@ -5,6 +5,7 @@ namespace App\Models\Construction;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class ConstructionModel extends Model
 {
@@ -21,4 +22,23 @@ class ConstructionModel extends Model
         'created_at',
         'updated_at'
     ];
+
+    protected static function booted()
+    {
+        static::deleting(function ($construction) {
+            if ($construction->photo && Storage::disk('public')->exists($construction->photo)) {
+                Storage::disk('public')->delete($construction->photo);
+            }
+        });
+
+        static::updating(function ($construction) {
+            if ($construction->isDirty('photo')) {
+                $oldPhoto = $construction->getOriginal('photo');
+
+                if ($oldPhoto && Storage::disk('public')->exists($oldPhoto)) {
+                    Storage::disk('public')->delete($oldPhoto);
+                }
+            }
+        });
+    }
 }
